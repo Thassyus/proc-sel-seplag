c2de2e8da80aa92304cb07bf10d1867cc505cbee
Thassyus Fabiano M A Carvalho <thassyus.fabiano@gmail.com>
2025-03-29 12:32:57 -0400

    implementado serviço abitus para acessar os dados da api

--- src/app/components/grid-fotos/grid-fotos.component.ts
+++ src/app/components/grid-fotos/grid-fotos.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AbitusService } from '../../abitus.service';
+import { RespostaPessoa } from '../../models.types';
 
 @Component({
   selector: 'app-grid-fotos',
   templateUrl: './grid-fotos.component.html',
   styleUrl: './grid-fotos.component.scss',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
 })
-export class GridFotosComponent {}
+export class GridFotosComponent {
+  private readonly _abitusService = inject(AbitusService);
+
+  listaPessoas: any = [];
+
+  ngOnInit(): void {
+    this._abitusService.listarPessoas().subscribe({
+      next: (listaPessoas: RespostaPessoa) => {
+        this.listaPessoas = listaPessoas;
+      },
+    });
+  }
+}
---
+++ src/app/models.types.ts
@@ -0,0 +1,53 @@
+export interface RespostaPessoa {
+  totalPages: number;
+  totalElements: number;
+  pageable: Paginavel;
+  numberOfElements: number;
+  first: boolean;
+  last: boolean;
+  size: number;
+  content: Pessoa[];
+  number: number;
+  sort: Ordenacao;
+  empty: boolean;
+}
+
+export interface Paginavel {
+  pageNumber: number;
+  pageSize: number;
+  sort: Ordenacao;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface Ordenacao {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
+export interface Pessoa {
+  id: number;
+  nome: string;
+  idade: number;
+  sexo: string;
+  vivo: true;
+  urlFoto: string;
+  ultimaOcorrencia: UltimaOcorrencia;
+}
+
+export interface UltimaOcorrencia {
+  dtDesaparecimento: string;
+  dataLocalizacao: string;
+  encontradoVivo: false;
+  localDesaparecimentoConcat: string;
+  ocorrenciaEntrevDesapDTO: ocorrenciaEntrevDesapDTO;
+  listaCartaz: string;
+  ocoId: number;
+}
+
+export interface ocorrenciaEntrevDesapDTO {
+  informacao: string;
+  vestimentasDesaparecido: string;
+}
